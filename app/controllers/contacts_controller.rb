2e96f889bda05a726c8f74daa9952d0556b100b5
Lionel Ouei <karlhengli@gmail.com>
2017-05-11 05:15:39 +0000

    Added actionmailer settings

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,7 +12,7 @@ class ContactsController < ApplicationController
             email = params[:contact][:email]
             body = params[:contact][:comments]
             
-            ContactMailer.Contact_email(name, email, body).deliver
+            ContactMailer.contact_email(name, email, body).deliver
             flash[:success] = 'Message sent'
             redirect_to new_contact_path 
         else
