3ec1a0685a63cc2c8b29bd1c4ed4e006f53edd20
Lionel Ouei <karlhengli@gmail.com>
2017-05-11 05:44:57 +0000

    Added actionmailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -12,5 +12,5 @@ ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRIG_PASSWORD'],
     :domain => 'heroku.com',
-    :enable_startsls_auto => true
+    :enable_startstls_auto => true
 }
