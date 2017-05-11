f6f9bff0d450558a76a81f5f7108a09d66e36db5
Lionel Ouei <karlhengli@gmail.com>
2017-05-11 17:48:12 +0000

    Revert "Email fixed"
    This reverts commit ef44a1ad70819e0ce58ce19dfec94f0abc171fb3.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
     
-    default to: 'karlhengli@gmail.com'
+    default to: 'linodior@gmail.com'
     def contact_email(name, email, body)
         @name = name
         @email = email
