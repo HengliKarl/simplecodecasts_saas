ef44a1ad70819e0ce58ce19dfec94f0abc171fb3
Lionel Ouei <karlhengli@gmail.com>
2017-05-11 16:34:12 +0000

    Email fixed

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ActionMailer::Base
     
-    default to: 'linodior@gmail.com'
+    default to: 'karlhengli@gmail.com'
     def contact_email(name, email, body)
         @name = name
         @email = email
